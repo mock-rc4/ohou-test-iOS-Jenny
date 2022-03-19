65b5deff8c06398a50aecd9827ab692fc1db0afc
0inn <bueaty1220@naver.com>
2022-03-19 20:41:13 +0900

    Bucketplace init

---
+++ Bucketplace/Bucketplace/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bucketplace
+//
+//  Created by 김영인 on 2022/03/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
