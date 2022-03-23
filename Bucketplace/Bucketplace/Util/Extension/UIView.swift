25f2450044806ca0dd5dd56d9fb4a251bcf446d0
0inn <bueaty1220@naver.com>
2022-03-23 19:10:29 +0900

    [+] UIView - border 관련 모듈 생성

---
+++ Bucketplace/Bucketplace/Util/Extension/UIView.swift
@@ -0,0 +1,41 @@
+//
+//  UIView.swift
+//  Bucketplace
+//
+//  Created by 김영인 on 2022/03/23.
+//
+
+import UIKit
+
+extension UIView {
+    
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
+    
+}
