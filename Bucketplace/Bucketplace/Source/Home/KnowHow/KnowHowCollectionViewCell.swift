dc0c0c37f03db60a00eb4bd82796db300794406e
0inn <bueaty1220@naver.com>
2022-03-30 11:51:37 +0900

    [#] 북마크 추가 및 삭제 API 연동

--- Bucketplace/Bucketplace/Source/Home/KnowHow/KnowHowCollectionViewCell.swift
+++ Bucketplace/Bucketplace/Source/Home/KnowHow/KnowHowCollectionViewCell.swift
@@ -7,26 +7,48 @@
 
 import UIKit
 
-class KnowHowCollectionViewCell: UICollectionViewCell {
+class KnowHowCollectionViewCell: BookMarkCollectionViewCell {
 
+    lazy var bookMarkDataManager = BookMarkDataManager()
     
     @IBOutlet weak var img: UIImageView!
     @IBOutlet weak var title: UILabel!
     @IBOutlet weak var name: UILabel!
     @IBOutlet weak var info: UILabel!
+    @IBOutlet weak var bookmarkBtn: UIButton!
+    
+    var feedId: Int!
+    var imgFlag: Bool = false
     
     override func awakeFromNib() {
         super.awakeFromNib()
     }
 
-    @IBAction func bookmarkBtnClick(_ sender: Any) {
-        print("📎 bookmark")
+    @IBAction func bookmarkBtnClick(_ btn: UIButton) {
+        print("📎 \(btn.tag)")
+        if imgFlag {
+            self.bookMarkDataManager.deleteBookMark(btn.tag, self)
+            btn.setBackgroundImage(UIImage(named: "bookmark"), for: .normal)
+            print(imgFlag)
+            imgFlag = false
+        } else {
+            self.bookMarkDataManager.postBookMark(btn.tag, self)
+            bookmarkBtn.setBackgroundImage(UIImage(named: "bookmark2"), for: .normal)
+            print(imgFlag)
+            imgFlag = true
+        }
     }
     
     func setData(_ data: KnowhowFeed) {
+        bookmarkBtn.tag = data.feedId
         Functions.shared.urlToImg(data.thumbnailUrl, img)
         title.text = data.description
         name.text = data.nickname
         info.text = "조회수 \(data.viewCount) · 스크랩 \(data.scrappedCount)"
+        if (data.isBookmarked == 1) {
+            imgFlag = true
+            bookmarkBtn.setBackgroundImage(UIImage(named: "bookmark2"), for: .normal)
+        }
     }
+    
 }
